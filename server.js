97feb1075aa7898e58e4eee7fd03e84a1c3bd58c
Ala2Hamdan <ala2hamdan92@gmail.com>
2021-04-04 22:12:14 +0300

    m

--- server.js
+++ server.js
@@ -1,28 +1,55 @@
 'use strict';
-const express =require('express');
-require('dotenv').config();
+const PORT = process.env.PORT || 3001;
+const express = require('express');
 const superagent = require('superagent');
-
-let app = express();
-const PORT =process.env.PORT || 3000 ;
-
-app.listen( PORT,()=>{
-    console.log(`this application listen to port${PORT}`);
-    });
+const path = require('path');
+const app = express();
+app.use(errorHandler);
+app.use(express.static(path.join(__dirname, "public/styles")));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.set('view engine', 'ejs');
-
-app.get('/hello',homePage);
-// app.get('/',(req ,res)=>res.send('hello '));
-app.get('/searches/new',searchFunction);
-
-
-
-function homePage(require,response){
-    response.render('pages/index');
+app.set("views", path.join(__dirname, "views"))
+app.get('/searches/new', (req, res) => {
+  res.render('pages/searches/new');
+});
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('pages/error', {
+    error: err
+  });
 }
+app.post('/searches', getData);
 
-function searchFunction(request ,response){
-    response.render('pages/searches/new');
+function Book(data) {
+  this.title = data.volumeInfo.title;
+  this.author = data.volumeInfo.authors.join(', ');
+  this.description = data.volumeInfo.description;
+  this.image = (data.volumeInfo.imageLinks) ? data.volumeInfo.imageLinks.smallThumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
 }
-
-
+function getData(req, res) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  if (req.body['search-by'] === 'title') {
+    url += `+intitle:${req.body['name']}`;
+  }
+  if (req.body['search-by'] === 'author') {
+    url += `+inauthor:${req.body['name']}`;
+  }
+  superagent.get(url).then(data => {
+      return data.body.items.map(element => new Book(element));
+    })
+    .then(results => res.render('pages/searches/show', {
+      searchResults: results
+    }));
+}
+app.get('*', (req, res) => {
+  res.status(404).send('Page not found');
+  console.log('page not found');
+});
+app.listen(PORT, () => {
+  console.log('Listening on ', PORT);
+});

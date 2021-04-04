1d6dfa1998ac50d21ff29065327bf17a0b0eac2d
Ala2Hamdan <ala2hamdan92@gmail.com>
2021-04-05 00:21:11 +0300

    lab10

--- server.js
+++ server.js
@@ -1,55 +1,64 @@
 'use strict';
-const PORT = process.env.PORT || 3001;
 const express = require('express');
 const superagent = require('superagent');
-const path = require('path');
+
 const app = express();
-app.use(errorHandler);
-app.use(express.static(path.join(__dirname, "public/styles")));
-app.use(express.urlencoded({
-  extended: true
-}));
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.use(errorFunction);
+
 app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, "views"))
-app.get('/searches/new', (req, res) => {
+app.get('/',homePage);
+
+app.get('/searches/new', formShow);
+
+function homePage(req ,res){
+  res.render('pages/index');
+}
+function formShow(req, res) {
   res.render('pages/searches/new');
+};
+app.post('/searches', createSearch);
+
+app.listen(PORT, () => { console.log('Listening on ', PORT);});
+app.get('*', (req, res) => { res.status(404).send('Page Not Found');
 });
-function errorHandler(err, req, res, next) {
+
+function errorFunction(err, req, res, next) {
   if (res.headersSent) {
     return next(err);
   }
   res.status(500);
-  res.render('pages/error', {
-    error: err
-  });
+  res.render('pages/error', { error: err });
 }
-app.post('/searches', getData);
 
-function Book(data) {
+
+
+function createSearch(req, res) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+      // return (req.body['search'] === 'title')? url += `+intitle:${req.body['name']}`: url += `+inauthor:${req.body['name']}`;
+  if (req.body['search'] === 'title') {
+    url += `+intitle:${req.body['name']}`;
+  }
+  if (req.body['search'] === 'author') {
+    url += `+inauthor:${req.body['name']}`;
+  }
+  superagent.get(url).then(data => {
+      return data.body.items.map(items => new Books(items));
+    })
+    .then(result => res.render('pages/searches/show', { searchResult: result }));
+}
+
+
+
+function Books(data) {
   this.title = data.volumeInfo.title;
   this.author = data.volumeInfo.authors.join(', ');
   this.description = data.volumeInfo.description;
   this.image = (data.volumeInfo.imageLinks) ? data.volumeInfo.imageLinks.smallThumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
 }
-function getData(req, res) {
-  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-  if (req.body['search-by'] === 'title') {
-    url += `+intitle:${req.body['name']}`;
-  }
-  if (req.body['search-by'] === 'author') {
-    url += `+inauthor:${req.body['name']}`;
-  }
-  superagent.get(url).then(data => {
-      return data.body.items.map(element => new Book(element));
-    })
-    .then(results => res.render('pages/searches/show', {
-      searchResults: results
-    }));
-}
-app.get('*', (req, res) => {
-  res.status(404).send('Page not found');
-  console.log('page not found');
-});
-app.listen(PORT, () => {
-  console.log('Listening on ', PORT);
-});

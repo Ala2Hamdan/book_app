6b75cbf7b1023c5e85890abc4cde463fa476dcc6
Ala2Hamdan <ala2hamdan92@gmail.com>
2021-04-04 19:43:45 +0300

    step2

--- server.js
+++ server.js
@@ -6,18 +6,23 @@ const superagent = require('superagent');
 let app = express();
 const PORT =process.env.PORT || 3000 ;
 
+app.listen( PORT,()=>{
+    console.log(`this application listen to port${PORT}`);
+    });
 app.set('view engine', 'ejs');
 
 app.get('/hello',homePage);
 // app.get('/',(req ,res)=>res.send('hello '));
+app.get('/searches/new',searchFunction);
 
-app.listen( PORT,()=>{
-console.log(`this application listen to port${PORT}`);
-});
+
 
 function homePage(require,response){
     response.render('pages/index');
 }
 
+function searchFunction(request ,response){
+    response.render('pages/searches/new');
+}
 
 

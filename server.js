4c51f32a3467d1b21485a784aeb11cd1fff0d962
Ala2Hamdan <ala2hamdan92@gmail.com>
2021-04-06 03:02:11 +0300

    lab13

--- server.js
+++ server.js
@@ -2,14 +2,19 @@
 const express = require('express');
 const superagent = require('superagent');
 
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const pg = require('pg');
+const client = new pg.Client( process.env.DATABASE_URL);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
+client.connect().then(()=>{
+  app.listen(PORT, () => { console.log('Listening on ', PORT);});
+})
 app.use(errorFunction);
 
 app.set('view engine', 'ejs');
@@ -18,14 +23,45 @@ app.get('/',homePage);
 app.get('/searches/new', formShow);
 
 function homePage(req ,res){
-  res.render('pages/index');
+  let sql = "SELECT * FROM books";
+  client.query(sql).then(data =>{
+    console.log(data.rows);
+    let number =data.rows.length;
+     res.render('pages/index',{bookResult:data.rows , num:number});
+    
+    
+  })
+
 }
 function formShow(req, res) {
   res.render('pages/searches/new');
 };
 app.post('/searches', createSearch);
+app.post('/',saveDataFrom);
+app.get('/books/:id',datailsFunction);
+ 
+function datailsFunction(req ,res){
+ let idD = req.params.id
+ let sql ="SELECT * FROM books WHERE id=$1"
+ 
+ client.query(sql,[idD]).then(element =>{
+  res.render('pages/books/detail', {detialView:element.rows[0]})
+ })
+}
 
-app.listen(PORT, () => { console.log('Listening on ', PORT);});
+function saveDataFrom(req ,res){
+ let info = req.body;
+ console.log(info);
+ let sql = "INSERT INTO books (author, title, isbn, image_url, description) Values ($1,$2,$3,$4,$5) RETURNING *";
+ let values =[info.author,info.title,'',info.img ,info.desc];
+ client.query(sql,values).then(result=>{
+  let idData = result.rows[0].id;
+  console.log(idData);
+   res.redirect(`/book/${idData}`)
+ })
+
+}
+
 app.get('*', (req, res) => { res.status(404).send('Page Not Found');
 });
 
@@ -44,9 +80,11 @@ function createSearch(req, res) {
 
 
 function Books(data) {
-  console.log(data);
+  // console.log(data);
   this.title = data.volumeInfo.title;
   this.author = data.volumeInfo.authors.join(', ');
   this.description = data.volumeInfo.description;
   this.image = (data.volumeInfo.imageLinks) ? data.volumeInfo.imageLinks.smallThumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
 }
+
+
